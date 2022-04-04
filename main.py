65b0e462550272cf60b8719b9b840e540ae913a7
Misha133 <mihagribkov133@gmail.com>
2022-04-04 21:36:59 +0300

    added guildIDs.json;

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from enums.LogLevel import LogLevel
 from sdamgia import SdamGIA
 import interactions
+from interactions.api.models.misc import MISSING
 from utils.Log import log
-
-TOKEN = ""
+import json
 
 
 with open("token.txt", mode='r', encoding='utf8') as f:
     TOKEN = f.read()
+
+with open("guildIDs.json", mode='r', encoding='utf8') as f:
+    data = json.loads(f.read())
+    if len(data['ids']) == 0:
+        GuildIDS = MISSING
+    else:
+        GuildIDS = data['ids']
+
 
 bot = interactions.Client(token=TOKEN)
 
@@ -21,3 +29,9 @@ async def on_ready():
 if __name__ == "__main__":
     bot.start()
 
+
+@bot.command(
+    name="status",
+    description="Get bot's status",
+    scope=
+)
