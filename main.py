35eb062403fa4db8924b0addfcbbd64a87ebf2f7
Misha133 <mihagribkov133@gmail.com>
2022-04-04 20:43:00 +0300

    removed crap

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from sdamgia import SdamGIA
+import interactions
+
+
+
+
 
 sdamgia = SdamGIA()
 
-subject = 'math'
-id = '1001'
-a = sdamgia.get_problem_by_id(subject, id)
-
-
 if __name__ == "__main__":
-    print(a)
