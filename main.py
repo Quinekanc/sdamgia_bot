38d72cd4e38a33dabe5a0985ef9fdde4ffc18b8e
Misha133 <mihagribkov133@gmail.com>
2022-04-04 21:26:11 +0300

    fixed fuck up with bot token; token already's reset

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def log(*args, loglevel=LogLevel.INFO):
 with open("token.txt", mode='r', encoding='utf8') as f:
     token = f.read()
 
-bot = interactions.Client(token='OTYwNTk1NDg2ODgzMDc0MDU5.YksuQw.mmp0Ac8UD6gHLZ8jYXorO2IpFXY')
+bot = interactions.Client(token=TOKEN)
 
 sdamgia = SdamGIA()
 
