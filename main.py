67507fb541e6c96b42ebbe25720da9e456850c68
Misha133 <mihagribkov133@gmail.com>
2022-04-04 20:53:18 +0300

    bot now runs

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from sdamgia import SdamGIA
 import interactions
 
 
+TOKEN = ""
 
 
+with open("token.txt", mode='r', encoding='utf8') as f:
+    token = f.read()
+
+
+bot = interactions.Client(token='OTYwNTk1NDg2ODgzMDc0MDU5.YksuQw.mmp0Ac8UD6gHLZ8jYXorO2IpFXY')
 
 sdamgia = SdamGIA()
 
+
 if __name__ == "__main__":
+    bot.start()
