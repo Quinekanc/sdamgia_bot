6d0c8a85088ff9d3570cc9d9b9844d20f9df28f8
Misha133 <mihagribkov133@gmail.com>
2022-04-09 00:33:00 +0300

    added white background to images

--- utils/ImageUtils.py
+++ utils/ImageUtils.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pyvips
 import uuid
+from PIL import Image
 
 imageFolder = "image_cache"
 
@@ -17,10 +18,15 @@ def GetPng(imageUrl: str):
     with open(f"{imageFolder}/{fileName}", mode="wb") as file:
         file.write(content)
 
-    img = pyvips.Image.new_from_file(f"{imageFolder}/{fileName}", access="sequential")
+    img = pyvips.Image.new_from_file(f"{imageFolder}/{fileName}", access="sequential", dpi=300)
     img.write_to_file(f"{imageFolder}/{fileName.replace('.svg', '.png')}")
 
     os.remove(f"{imageFolder}/{fileName}")
+
+    image = Image.open(f"{imageFolder}/{fileName.replace('.svg', '.png')}")
+    background = Image.new('RGBA', image.size, color="white")
+    Image.alpha_composite(background, image)\
+        .save(f"{imageFolder}/{fileName.replace('.svg', '.png')}")
 
     with open(f"{imageFolder}/{fileName.replace('.svg', '.png')}", mode="rb") as file:
         uploadFile = {"file": file}
